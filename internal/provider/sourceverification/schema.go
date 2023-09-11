93082686380b08ae5aa53bda5b7ac813cc833034
Alex Luong <alex@alexluong.com>
2023-09-12 02:14:18 +0700

    fix: Validate exactly one source verification configuration

--- internal/provider/sourceverification/schema.go
+++ internal/provider/sourceverification/schema.go
@@ -2,11 +2,13 @@ package sourceverification
 
 import (
 	"context"
+	"terraform-provider-hookdeck/internal/validators"
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 )
 
 func (r *sourceVerificationResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
@@ -52,6 +54,9 @@ func (r *sourceVerificationResource) Schema(_ context.Context, _ resource.Schema
 					"workos":          workOSConfigSchema(),
 					"xero":            xeroConfigSchema(),
 					"zoom":            zoomConfigSchema(),
+				},
+				Validators: []validator.Object{
+					validators.ExactlyOneChild(),
 				},
 				Description: "The verification configs for the specific verification type",
 			},
