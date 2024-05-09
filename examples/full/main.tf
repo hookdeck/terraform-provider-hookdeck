4b485239e26578e530e1ecfc7186cbfa538f2ed3
Phil Leggetter <phil@leggetter.co.uk>
2024-05-09 15:11:08 +0100

    chore: add full example using variables

---
+++ examples/full/main.tf
@@ -0,0 +1,45 @@
+variable "HOOKDECK_API_KEY" {
+  type = string
+}
+
+variable "HEADER_FILTER_VALUES" {
+  type = list(string)
+}
+
+terraform {
+  required_providers {
+    hookdeck = {
+      source = "hookdeck/hookdeck"
+      version = "~> 0.3.1"
+    }
+  }
+}
+
+provider "hookdeck" {
+  api_key = var.HOOKDECK_API_KEY
+}
+
+resource "hookdeck_source" "my_source" {
+  name = "my_source"
+}
+
+resource "hookdeck_destination" "my_destination" {
+  name = "my_destination"
+  url  = "https://mock.hookdeck.com"
+}
+
+resource "hookdeck_connection" "my_connection" {
+  source_id      = hookdeck_source.my_source.id
+  destination_id = hookdeck_destination.my_destination.id
+  rules = [
+    {
+      filter_rule = {
+        headers = {
+          json = jsonencode({
+            x-event-type = { "$or" : var.HEADER_FILTER_VALUES }
+          })
+        } 
+      } 
+    }
+  ]
+}
