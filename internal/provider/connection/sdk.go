2ae9f42299423831c9388e46a595857680abec64
David Jimenez <dvejmz@sgfault.com>
2025-10-24 16:58:01 +0100

    feat: add response status codes attribute to connection retry strategy

--- internal/provider/connection/sdk.go
+++ internal/provider/connection/sdk.go
@@ -409,6 +409,15 @@ func rulesToAPI(ctx context.Context, rules []rule) ([]interface{}, error) {
 				rule["interval"] = ruleItem.RetryRule.Interval.ValueInt64()
 			}
 
+			responseStatusCodesFieldsSet := !ruleItem.RetryRule.ResponseStatusCodes.IsNull() && !ruleItem.RetryRule.ResponseStatusCodes.IsUnknown()
+			if responseStatusCodesFieldsSet {
+				responseStatusCodes := []string{}
+				ruleItem.RetryRule.ResponseStatusCodes.ElementsAs(ctx, &responseStatusCodes, false)
+				if len(responseStatusCodes) > 0 {
+					rule["response_status_codes"] = responseStatusCodes
+				}
+			}
+
 			result = append(result, rule)
 		}
 
@@ -553,6 +562,19 @@ func rulesFromAPI(rules []interface{}) []rule {
 				retryRule.Interval = types.Int64Value(int64(interval))
 			} else {
 				retryRule.Interval = types.Int64Null()
+			}
+			if responseStatusCodes, ok := ruleMap["response_status_codes"].([]interface{}); ok {
+				statusCodeExpressions := []types.String{}
+				for _, expression := range responseStatusCodes {
+					if expressionStr, ok := expression.(string); ok {
+						statusCodeExpressions = append(statusCodeExpressions, types.StringValue(expressionStr))
+					}
+				}
+				if len(statusCodeExpressions) > 0 {
+					retryRule.ResponseStatusCodes, _ = types.ListValueFrom(context.Background(), types.StringType, statusCodeExpressions)
+				} else {
+					retryRule.ResponseStatusCodes = types.ListNull(types.StringType)
+				}
 			}
 
 			result = append(result, rule{RetryRule: retryRule})
