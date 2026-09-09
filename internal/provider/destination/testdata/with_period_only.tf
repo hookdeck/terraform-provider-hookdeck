3398729f34b172f318ef0f30871dba2ca502bdb5
Alexandre Bouchard <alexbouchardd@gmail.com>
2026-09-09 13:17:41 -0400

    fix: Fix missing comma

--- internal/provider/destination/testdata/with_period_only.tf
+++ internal/provider/destination/testdata/with_period_only.tf
@@ -2,7 +2,7 @@ resource "hookdeck_destination" "test_%[1]s" {
   name = "test-destination-%[1]s"
   type = "HTTP"
   config = jsonencode({
-    url               = "https://mock.hookdeck.com",
+    url = "https://mock.hookdeck.com",
     delivery_policy = {
       period = "concurrent"
     }
